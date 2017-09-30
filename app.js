94299cf8c71ead86f304a20d14fd03918b3efa06
mohammad <you@example.com>
2017-09-30 17:06:01 -0400

    added html content

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
+const c = console.log;
+
 
 app.use('/', express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -8,5 +11,11 @@ app.use(bodyParser.json());
 
 
 app.get("/",(req,res)=>{
+	res.sendFile(__dirname + "/index.html");
+})
 
+
+
+app.listen(port,(err)=>{
+	err?c("err:",err):c("listening on:",port);
 })
